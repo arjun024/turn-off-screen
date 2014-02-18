0375d14cff866cfaf860fc5244cfcb3efacda06f
Arjun Sreedharan <arjun024@gmail.com>
2014-02-18 22:21:24 +0530

    works on both linux and windows

---
+++ turnoff.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+import sys
+if sys.platform.startswith('linux'):
+	import os
+	os.system("xset dpms force off")
+elif sys.platform.startswith('win'):
+	import win32gui
+	import win32con
+	SC_MONITORPOWER = 0xF170
+	win32gui.SendMessage(win32con.HWND_BROADCAST, win32con.WM_SYSCOMMAND, SC_MONITORPOWER, 2)
