e96cacaeb84b7d686c84c6b13cc2cd2c852b6321
Arjun Sreedharan <arjun.sreedharan@sap.com>
2014-02-19 15:42:54 +0530

    add case for mac given by rein

---
+++ test/turnoff-test.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+import sys
+if sys.platform.startswith('linux'):
+	import os
+	os.system("xset dpms force off")
+elif sys.platform.startswith('win'):
+	import win32gui
+	import win32con
+	SC_MONITORPOWER = 0xF170
+	win32gui.SendMessage(win32con.HWND_BROADCAST, win32con.WM_SYSCOMMAND, SC_MONITORPOWER, 2)
+elif sys.platform.startswith('darwin'):
+	import subprocess
+	subprocess.call('echo "tell application \"Finder\" to sleep" | osascript ', shell=True)
