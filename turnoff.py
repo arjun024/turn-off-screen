8e1676da24562cbc199db84381de9f64d14d5d95
frytaz <frytaz@gmail.com>
2014-02-19 11:51:27 +0100

    osx support
    10.9+

--- turnoff.py
+++ turnoff.py
@@ -8,3 +8,6 @@ elif sys.platform.startswith('win'):
 	import win32con
 	SC_MONITORPOWER = 0xF170
 	win32gui.SendMessage(win32con.HWND_BROADCAST, win32con.WM_SYSCOMMAND, SC_MONITORPOWER, 2)
+elif sys.platform.startswith('darwin'):
+        import os
+        os.system("pmset displaysleepnow")
