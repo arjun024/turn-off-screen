c17f98bc4112433615c9e0b10bf73e5512eaceca
frytaz <frytaz@gmail.com>
2014-02-24 12:16:28 +0100

    Formatting

--- platform-specific/turnoff_darwin.c
+++ platform-specific/turnoff_darwin.c
@@ -2,14 +2,14 @@
 #include <IOKit/IOKitLib.h>
 
 /* Returns 0 on success and 1 on failure. */
-int main(void)
-{
+int main(void) {
     io_registry_entry_t reg = IORegistryEntryFromPath(kIOMasterPortDefault, "IOService:/IOResources/IODisplayWrangler");
     if (reg) {
-                IORegistryEntrySetCFProperty(reg, CFSTR("IORequestIdle"), kCFBooleanTrue);
-                IOObjectRelease(reg);
-        } else {
-                return 1;
-        }
-        return 0;
+        IORegistryEntrySetCFProperty(reg, CFSTR("IORequestIdle"), kCFBooleanTrue);
+        IOObjectRelease(reg);
+    } else {
+        return 1;
+    }
+
+    return 0;
 }
