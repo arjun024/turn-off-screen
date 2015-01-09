068dbe44ac12512c0d3c38d238d7ea1cc1f5c6f1
Arjun Sreedharan <arjun024@gmail.com>
2015-01-09 12:59:43 +0530

    merge win8.1 changes to main line
    In fa1bfca610a4a8bfbd18a82d9c81cc3033b7649c, @Wikilicious solved win 8.1.
    Merge it with mainline.

--- turnoff.py
+++ turnoff.py
@@ -6,8 +6,18 @@ if sys.platform.startswith('linux'):
 elif sys.platform.startswith('win'):
 	import win32gui
 	import win32con
+	from os import getpid, system
+	from threading import Timer
+	
+	def force_exit():
+		pid = getpid()
+		system('taskkill /pid %s /f' % pid)
+	
+	t = Timer(1, force_exit)
+	t.start()
 	SC_MONITORPOWER = 0xF170
 	win32gui.SendMessage(win32con.HWND_BROADCAST, win32con.WM_SYSCOMMAND, SC_MONITORPOWER, 2)
+	t.cancel()
 elif sys.platform.startswith('darwin'):
 	import subprocess
 	subprocess.call('echo \'tell application "Finder" to sleep\' | osascript', shell=True)
