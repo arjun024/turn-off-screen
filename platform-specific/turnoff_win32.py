260c7ecb34dfd38553d53b5245797ab51c10a1b5
hagrid67 <jdhwatson@gmail.com>
2022-03-19 19:34:40 +0000

    use SendMessageTimeout instead of SendMessage to avoid blocking

--- platform-specific/turnoff_win32.py
+++ platform-specific/turnoff_win32.py
@@ -2,4 +2,9 @@ import win32gui
 import win32con
 
 SC_MONITORPOWER = 0xF170
-win32gui.SendMessage(win32con.HWND_BROADCAST, win32con.WM_SYSCOMMAND, SC_MONITORPOWER, 2)
+# win32gui.SendMessage(win32con.HWND_BROADCAST, win32con.WM_SYSCOMMAND, SC_MONITORPOWER, 2)
+win32gui.SendMessageTimeout(win32con.HWND_BROADCAST,
+    win32con.WM_SYSCOMMAND, 
+    SC_MONITORPOWER, 2, 
+    win32con.SMTO_NOTIMEOUTIFNOTHUNG, 
+    1000)
--- turnoff.py
+++ turnoff.py
@@ -8,18 +8,12 @@ if sys.platform.startswith('linux'):
 elif sys.platform.startswith('win'):
 	import win32gui
 	import win32con
-	from os import getpid, system
-	from threading import Timer
-	
-	def force_exit():
-		pid = getpid()
-		system('taskkill /pid %s /f' % pid)
-	
-	t = Timer(1, force_exit)
-	t.start()
 	SC_MONITORPOWER = 0xF170
-	win32gui.SendMessage(win32con.HWND_BROADCAST, win32con.WM_SYSCOMMAND, SC_MONITORPOWER, 2)
-	t.cancel()
+	win32gui.SendMessageTimeout(win32con.HWND_BROADCAST,
+		win32con.WM_SYSCOMMAND, 
+		SC_MONITORPOWER, 2, 
+		win32con.SMTO_NOTIMEOUTIFNOTHUNG, 
+		1000)
 
 elif sys.platform.startswith('darwin'):
 	import subprocess
